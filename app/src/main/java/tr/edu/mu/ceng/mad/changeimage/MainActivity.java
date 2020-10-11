83ae4a453f5debe4cdd374d6cff35c83867e77aa
U170709027 <kursunsoztuana@gmail.com>
2020-10-12 00:01:59 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/changeimage/MainActivity.java
@@ -0,0 +1,24 @@
+package tr.edu.mu.ceng.mad.changeimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void ChangeImage (View view){
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.kuyruklu);
+    }
+
+
+}
